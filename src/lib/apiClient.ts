7dc090e778f202473accbdc70562182a18afd3a0
kingfrog <tono82g@gmail.com>
2024-08-06 22:41:04 +0900

    baseURL

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 import { headers } from "next/headers";
 
 const apiClient =axios.create({
-  baseURL:"http://localhost:8000/api",
+  // baseURL:"http://localhost:8000/api",
+  baseURL: process.env.NEXT_PUBLIC_API_BASEURL,
   headers:{
 "Content-type" : "application/json",
 // "Authorization" : `Bearer {token}`
